ec367ba042f5dcd213a6704203d18e49f2fcd2c3
yalnie <232255780+yalnie@users.noreply.github.com>
2026-05-09 13:00:24 +0300

    fix app

--- sw.js
+++ sw.js
@@ -2,18 +2,35 @@ const CACHE_NAME = 'new-way-v1';
 const ASSETS = [
   './',
   './index.html',
-  './app.js',
+  './js/app.js',
+  './css/style.css',
+  './css/light.css',
+  './css/dark.css',
   './manifest.json'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
   );
 });
