aea7722c0da5a9087f3772f501adf67126b651be
yalnie <232255780+yalnie@users.noreply.github.com>
2026-08-20 13:57:59 +0300

    nuke

--- sw.js
+++ sw.js
@@ -1,20 +1,6 @@
-const CACHE_NAME = 'new-way-v4';
-const ASSETS = [
-  './',
-  './index.html',
-  './js/app.js',
-  './css/style.css',
-  './css/light.css',
-  './css/dark.css',
-  './assets/site.webmanifest',
-  'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap',
-  'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined'
-];
+const CACHE_NAME = 'new-way-v5';
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
   self.skipWaiting();
 });
 
@@ -24,18 +10,16 @@ self.addEventListener('activate', (e) => {
       return Promise.all(
         keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
       );
-    })
+    }).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request).catch(() => {
-        if (e.request.mode === 'navigate') {
-          return caches.match('./index.html');
-        }
-      });
+    fetch(e.request).catch(() => {
+      if (e.request.mode === 'navigate') {
+        return caches.match(e.request);
+      }
     })
   );
 });
